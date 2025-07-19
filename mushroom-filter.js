908496d0520c5bf59a4808dd104a7c6001eab6e7
Joshua Stone <jstone925314@gmail.com>
2025-07-19 11:08:08 -0700

    Progressive enhancement via view transition animations

--- mushroom-filter.js
+++ mushroom-filter.js
@@ -9,14 +9,24 @@ const currentFilters = {
   edible: "all",
 };
 
+cards.forEach((card, idx) => {
+    const mushroomId = `mushroom-${idx + 1}`;
+    card.style.viewTransitionName = `card-${mushroomId}`;
+})
+
 seasonalFilter.addEventListener("change", updateFilter);
 edibleFilter.addEventListener("change", updateFilter);
 
 function updateFilter(e) {
   const filterType = e.target.name;
-
   currentFilters[filterType] = e.target.value;
-  filterCards();
+
+  if(!document.startViewTransition) {
+    // if View Transitions are not supported, fallback to a simple filter
+    filterCards();
+    return;
+  }
+  document.startViewTransition(() => filterCards());
 }
 
 function filterCards() {
@@ -25,10 +35,6 @@ function filterCards() {
   cards.forEach((card) => {
     const season = card.querySelector("[data-season]").dataset.season;
     const edible = card.querySelector("[data-edible]").dataset.edible;
-
-    const matchesSeason = currentFilters.season === season;
-
-    const matchesEdible = currentFilters.edible === edible;
 
     if (
       (currentFilters.season === "all" || currentFilters.season === season) &&
